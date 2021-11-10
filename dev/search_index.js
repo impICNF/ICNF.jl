65fd03700f4b7b32928bea4dfa6c6acc62eaa3e8
Documenter.jl <documenter@juliadocs.github.io>
2021-11-10 15:16:26 +0000

    build based on 3d9bbbb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ICNF","category":"page"},{"location":"#ICNF","page":"Home","title":"ICNF","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ICNF.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ICNF]","category":"page"}]
+}
